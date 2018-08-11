8a3d9cb16c8b8270d954414f4a3196beee8bb12e
silveryC <hello.shengC@gmail.com>
2018-08-11 21:03:59 +0800

    。

---
+++ prj/pages/todo/todo.js
@@ -0,0 +1,93 @@
+// pages/todo/todo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    navData: [
+      {
+        name: "待做",  //文本
+        current: 1,    //是否是当前页，0不是  1是
+        ico: 'icon-bijiben',  //不同图标
+        fn: 'gotoToDo'   //对应处理函数
+      }, {
+        name: "日程",
+        current: 0,
+        ico: 'icon-shufa',
+        fn: 'gotoSchedule'
+      }, {
+        name: "设置",
+        current: 0,
+        ico: 'icon-wenju',
+        fn: 'gotoSettings'
+      }
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  gotoSchedule: function () {
+    wx.navigateTo({
+      url: '../schedule/schedule',
+    })
+  },
+  gotoSettings: function () {
+    wx.navigateTo({
+      url: '../settings/settings',
+    })
+  }
+})
